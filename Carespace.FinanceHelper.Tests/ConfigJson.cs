309bad6b59e66fa71a85d1e83fd8cb5584cad978
Вадим Боровиков <vadim.borovikov@gmail.com>
2022-11-12 00:37:21 +0200

    Revert "Finance Helper removed"
    This reverts commit 2c45b7c423c59c88ceb5a276fb2faab71f535fdd.

---
+++ Carespace.FinanceHelper.Tests/ConfigJson.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using GoogleSheetsManager;
+using GryphonUtilities;
+using Newtonsoft.Json;
+
+namespace Carespace.FinanceHelper.Tests;
+
+internal sealed class ConfigJson
+{
+    public sealed class ShareJson : IConvertibleTo<Share>
+    {
+        [JsonProperty]
+        public string? Agent { get; set; }
+        [JsonProperty]
+        public string? Promo { get; set; }
+        [JsonProperty]
+        public bool? PromoForNet { get; set; }
+        [JsonProperty]
+        public decimal? Limit { get; set; }
+        [JsonProperty]
+        public decimal Value { get; set; }
+        [JsonProperty]
+        public decimal? ValueAfterLimit { get; set; }
+
+        public Share Convert()
+        {
+            string agent = Agent.GetValue(nameof(Agent));
+            decimal value = Value.GetValue(nameof(Value));
+            return new Share(agent, ValueAfterLimit, Promo, PromoForNet, Limit, value);
+        }
+    }
+
+    [JsonProperty]
+    public decimal? TaxFeePercent { get; set; }
+
+    [JsonProperty]
+    public decimal? DigisellerFeePercent { get; set; }
+
+    [JsonProperty]
+    public Dictionary<Transaction.PayMethod, decimal>? PayMasterFeePercents { get; set; }
+
+    [JsonProperty]
+    public Dictionary<string, List<ShareJson>>? ShareJsons { get; set; }
+}
---
+++ Carespace.FinanceHelper/Data/Digiseller/PurchaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Carespace.FinanceHelper.Data.Digiseller;
+
+public sealed class PurchaseResponse
+{
+    public sealed class ContentInfo
+    {
+        [JsonProperty]
+        public string? PromoCode { get; set; }
+    }
+
+    [JsonProperty]
+    public ContentInfo? Content { get; set; }
+}
